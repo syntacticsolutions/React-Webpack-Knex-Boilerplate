f04ebcc74337db734934012a2b5d8bb7f6b7a7d1
Miguel Cabrera <miguel@weareflagship.com>
2017-11-18 05:46:57 -0800

    made production version of web pack output and cleaned up the package son.

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -90,6 +90,9 @@ module.exports = {
         }, {
             test: /\.(ttf|eot|svg)(\?[a-z0-9#=&.]+)?$/,
             loader: 'file'
+        },  {
+            test: /\.css$/,
+            loaders: [ 'style-loader', 'css-loader' ]
         }]
     },
     postcss: [
