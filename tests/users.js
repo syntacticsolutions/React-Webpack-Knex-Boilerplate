dcb64c896fcf8db4e7d77e32a85327663e23efed
Miguel Cabrera <syntacticsolutions@gmail.com>
2017-11-19 13:47:39 -0800

    Moved unit tests into separate '/tests' folder

--- tests/users.js
+++ tests/users.js
@@ -1,11 +1,67 @@
-var expect  = require('chai').expect;
-var request = require('request');
+'use strict';
+const app = require('../backend.js');
+const chai = require('chai');
+const expect = chai.expect;
+chai.use(require('chai-http'));
+const tester = chai.request(app);
 
-it('Main page content', function(done) {
-    request('http://localhost:7555' , function(error, response, body) {
-        console.log(error);
-        console.log(response);
-        console.log(body);
-        done();
+// Our app
+
+describe('API endpoint /users', function() {
+    this.timeout(5000); // How long to wait for a response (ms)
+
+    // GET - List all colors
+    it('should return all users', function() {
+
+        return tester.get('/api/users')
+
+        .then(function(res) {
+            expect(res).to.have.status(200);
+            expect(res).to.be.json;
+            expect(res.body).to.be.an('object');
+            expect(res.body.results).to.be.an('array');
+        });
+    });
+
+    // GET - Invalid path
+    it('should return Not Found', function() {
+        return tester
+            .get('/INVALID_PATH')
+            .then(function(res) {
+                throw new Error('Path exists!');
+            })
+            .catch(function(err) {
+                expect(err).to.have.status(404);
+            });
+    });
+
+    // POST - Add new color
+    it('should add new color', function() {
+        return tester
+            .post('/colors')
+            .send({
+                color: 'YELLOW'
+            })
+            .then(function(res) {
+                expect(res).to.have.status(201);
+                expect(res).to.be.json;
+                expect(res.body).to.be.an('object');
+                expect(res.body.results).to.be.an('array').that.includes(
+                    'YELLOW');
+            });
+    });
+
+    // POST - Bad Request
+    it('should return Bad Request', function() {
+        return tester.post('/colors')
+            .send({
+                color: 'YELLOW'
+            })
+            .then(function(res) {
+                throw new Error('Invalid content type!');
+            })
+            .catch(function(err) {
+                expect(err).to.have.status(400);
+            });
     });
 });
