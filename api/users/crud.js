0b7db7dbe05983be91dbd930bb8cebba7c689a8d
Miguel Cabrera <miguel@weareflagship.com>
2017-11-18 19:43:59 -0800

    setup vscode launch.json for debugging and added list and get functions to endpoint.

--- api/users/crud.js
+++ api/users/crud.js
@@ -1,10 +1,10 @@
-var knex = require('knex');
-var validate = require('validate');
+var sql = require('../sql');
+var validate = require('../validate');
 
 // define validation object parameters
 
 const users = {
-	id: {type: validate.TYPE_INT }
+	id: {type: validate.TYPE_INT },
 	first_name: { type: validate.TYPE_STR, length: 20 },
 	last_name: { type: validate.TYPE_STR, length: 20 },
 	address: { type: validate.TYPE_ADDR, length:35 },
@@ -13,16 +13,36 @@ const users = {
 	zip: { type: validate.TYPE_INT, length: 5 }
 }
 
+//Expose CRUD functions for endpoint.
+
 
 module.exports = {
 
 	list: (req, res) => {
 
-  		res.status(200).json({success:true});
+		return sql.select().from('users')
+		.then(data => {
+
+			if(!data[0])
+				return res.sendStatus(404)
+
+			return res.status(200).json(data);
+		})
+  		
 	},
 
 	get: (req, res) => {
-		res.status(200).json({success:true});
+		return sql.select('*')
+		.from('users')
+		.where({
+			'id':req.params.id
+		})
+		.then(data =>{
+
+			if(!data[0]) return res.sendStatus(404);
+
+			return res.status(200).send(data[0]);
+		})
 	},
 
 	upsert: (req, res) => {
