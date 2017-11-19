b090d9ac92403366be5b91914dabf9e3be91b4b6
Miguel Cabrera <miguel@weareflagship.com>
2017-11-19 08:41:05 -0800

    cleaned up validation script

--- api/validate.js
+++ api/validate.js
@@ -24,39 +24,39 @@ module.exports = {
 
 		_.each(model, (val, key)=>{
 
-			if(val.type == 'INT'){
+			if(val.type === 'INT'){
 				let num = parseInt(data[key]);
 				if( num < 1 || !isNAN(num)){
 					return res.sendStatus(412).send('Error: ' + data[key] + ' is not a valid integer.');
 				}
 			}
 
-			if(val.type == 'ADDRESS'){
+			if(val.type === 'ADDRESS'){
 				if(data[key].match(/^\s*\S+(?:\s+\S+){2}/) === null){
 					return res.sendStatus(412).send('Error: ' + data[key] + ' is not a valid address.');
 				}
 			}
 
-			if(val.type == 'NAME'){
-				if(/^[-A-Z ]+$/i.test(data[key]) == false){
+			if(val.type === 'NAME'){
+				if(/^[-A-Z ]+$/i.test(data[key]) === false){
 					return res.sendStatus(412).send('Error: ' + data[key] + ' is not a valid name.');
 				}
 			}
 
-			if(val.type == 'ZIPCODE'){
+			if(val.type === 'ZIPCODE'){
 				let strNum = data[key].toString();
 				if(strNum.length < 5 || strNum.length > 5 || !isNaN(strNum)){
-					return res.sendStats(412).send('Error: ' + data[key] + ' is not a valid zip code.')
+					return res.sendStats(412).send('Error: ' + data[key] + ' is not a valid zip code.');
 				}
 			}
 
 			if(data[key].length > val.length){
-				return res.sendStatus(412).send('Error: ' + key + ' must be less than ' + val.length ' characters long.');
+				return res.sendStatus(412).send('Error: ' + key + ' must be less than ' + val.length + ' characters long.');
 			}
 
 			if(val.required) {
 				if(data[key].length < 1){
-					return res.sendStatus(412).send('Error: ' + key + ' cannot be empty.')
+					return res.sendStatus(412).send('Error: ' + key + ' cannot be empty.');
 				}
 			}
 		})
